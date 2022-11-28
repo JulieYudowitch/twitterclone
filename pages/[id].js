deebf579232c899b95ddc052fb6b7f1da5a86d63
julie yudowitch <julieyudo@gmail.com>
2022-11-28 17:11:34 -0500

    attempt to fix dynamic routing

--- pages/[id].js
+++ pages/[id].js
@@ -5,16 +5,20 @@ import Sidebar from '../components/Sidebar'
 import SinglePost from "../components/SinglePost";
 import Trending from "../components/Trending";
 import { AppContext } from "../context/AppContext";
+import { useRouter } from "next/router";
 
 export default function PostPage() {
+  const router = useRouter();
   const [appContext] = useContext(AppContext)
   return (
-    <div>
+    (router.isReady && <div>
       <Head>
         <title>Tweet reply</title>
         <meta name="description" content="Reply to tweet"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      
       <main className="relative max-w-[1400px] mx-auto">
         <Sidebar />
         <div className="flex gap-6">
@@ -23,6 +27,6 @@ export default function PostPage() {
           {appContext?.isModalOpen && <Modal />}
         </div>
       </main>
-    </div>
+    </div>)
   );
 }
